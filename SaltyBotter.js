cead6cd3c2e591afe83b9bd508fbada35f4dc212
OSad <andreg.95@gmail.com>
2021-02-03 18:31:20 -0300

    Added exhibition mode detection
    also cleared a bunch of console.logs

--- SaltyBotter.js
+++ SaltyBotter.js
@@ -1,18 +1,17 @@
 // ==UserScript==
-// @name        New script - saltybet.com
+// @name        SaltyBotter - saltybet.com
 // @namespace   Violentmonkey Scripts
 // @match       https://www.saltybet.com/
 // @grant       none
-// @version     1.0
-// @author      -
+// @version     1.1
+// @author      OSad
 // @description 2/2/2021, 6:37:21 PM
 // ==/UserScript==
 
 
-//window.addEventListener("load", function(){
-  
 let percentageNumberRegex = /[0-9][0-9]%/;
 let strippedDownNumberRegex = /[0-9][0-9]/;
+let exhibitionGameModeRegex = /exhibition/;
 let percentageNumberRedTeam = 0;
 let strippedDownNumberRedTeam = 0;
 let percentageNumberBlueTeam = 0;
@@ -22,47 +21,90 @@ let betAmount = 0;
 
 
 
-window.addEventListener('load', function () {
-  //alert("It's loaded!")
-  
-var intervalID = window.setInterval(myCallback, 30000);
-  
-  
-  function myCallback()
-{
+window.addEventListener('load', function() {
+    //alert("It's loaded!")
 
-  console.log(document.getElementById("bettors1").innerHTML);
-  console.log(percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML));
-    
-  percentageNumberRedTeam = percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML);
-  strippedDownNumberRedTeam = strippedDownNumberRegex.exec(percentageNumberRedTeam);
-  console.log(strippedDownNumberRedTeam);
-  
-  console.log(document.getElementById("bettors2").innerHTML);
-  console.log(percentageNumberRegex.exec(document.getElementById("bettors2").innerHTML));
-  percentageNumberBlueTeam = percentageNumberRegex.exec(document.getElementById("bettors2").innerHTML);
-  strippedDownNumberBlueTeam = strippedDownNumberRegex.exec(percentageNumberBlueTeam);
-  console.log(strippedDownNumberBlueTeam);
-    
-  cashAmount = document.getElementById("balance").textContent;
-  console.log(cashAmount);
-  cashAmount = parseFloat(cashAmount.replace(/,/g, ''));
-  console.log(cashAmount);
-  betAmount = Math.round(0.01 * cashAmount);
-  
-    
-  if (strippedDownNumberRedTeam > strippedDownNumberBlueTeam)
-    {
-       document.getElementById("wager").value = betAmount;
-       document.getElementById("player1").click(); 
-    }
-  else if (strippedDownNumberRedTeam < strippedDownNumberBlueTeam)
-    {
-       document.getElementById("wager").value = betAmount;
-       document.getElementById("player2").click(); 
-    }
-  else {}
+    var intervalID = window.setInterval(myCallback, 30000);
 
-}
+
+    function myCallback() {
+
+
+        if (exhibitionGameModeRegex.test(document.getElementById("footer-alert").textContent) === true) 
+        {
+          
+            console.clear();
+            console.log("Exhibs detected! Betting on the favorite!");
+
+
+            percentageNumberRedTeam = percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML);
+            strippedDownNumberRedTeam = strippedDownNumberRegex.exec(percentageNumberRedTeam);
+
+            percentageNumberBlueTeam = percentageNumberRegex.exec(document.getElementById("bettors2").innerHTML);
+            strippedDownNumberBlueTeam = strippedDownNumberRegex.exec(percentageNumberBlueTeam);
+
+            cashAmount = document.getElementById("balance").textContent;
+            cashAmount = parseFloat(cashAmount.replace(/,/g, ''));
+            betAmount = Math.round(0.01 * cashAmount);
+
+
+          
+            if (strippedDownNumberRedTeam > strippedDownNumberBlueTeam) 
+            {
+              
+                document.getElementById("wager").value = betAmount;
+                document.getElementById("player1").click();
+
+            } 
+          
+            else if (strippedDownNumberRedTeam < strippedDownNumberBlueTeam) 
+            
+            {
+                document.getElementById("wager").value = betAmount;
+                document.getElementById("player2").click();
+
+            } 
+          
+            else {}
+        } 
+      
+      
+        else 
+        
+          {
+            console.clear();
+            console.log("Normal matches detected! Betting on the underdog.");
+
+            percentageNumberRedTeam = percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML);
+            strippedDownNumberRedTeam = strippedDownNumberRegex.exec(percentageNumberRedTeam);
+
+            percentageNumberBlueTeam = percentageNumberRegex.exec(document.getElementById("bettors2").innerHTML);
+            strippedDownNumberBlueTeam = strippedDownNumberRegex.exec(percentageNumberBlueTeam);
+
+            cashAmount = document.getElementById("balance").textContent;
+            cashAmount = parseFloat(cashAmount.replace(/,/g, ''));
+            betAmount = Math.round(0.01 * cashAmount);
+
+
+            if (strippedDownNumberRedTeam > strippedDownNumberBlueTeam) 
+            {
+              
+                document.getElementById("wager").value = betAmount;
+                document.getElementById("player2").click();
+
+            } 
+          
+            else if (strippedDownNumberRedTeam < strippedDownNumberBlueTeam) 
+            {
+              
+                document.getElementById("wager").value = betAmount;
+                document.getElementById("player1").click();
+
+            } 
+          
+            else {}
+        }
+
+    }
 
 })
