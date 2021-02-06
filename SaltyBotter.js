ab4e3b49753a24603b25db09b63d30779adc4f17
OSad <andreg.95@gmail.com>
2021-02-06 02:43:33 -0300

    Removed extra elements from the page
    Also shuffled the size and position of some text, and included a console log of number of matches until tournament mode.

--- SaltyBotter.js
+++ SaltyBotter.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.saltybet.com/
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      OSad
 // @description 2/2/2021, 6:37:21 PM
 // ==/UserScript==
@@ -12,6 +12,9 @@
 let percentageNumberRegex = /[0-9][0-9]%/;
 let strippedDownNumberRegex = /[0-9][0-9]/;
 let exhibitionGameModeRegex = /exhibition/;
+let tournamentGameModeStartRegex = /Tournament mode start!/;
+let tournamentGameModeRegexPlural = /[0-9][0-9] characters are left in the bracket!/;
+let tournamentGameModeRegexSingular = /[0-9] characters are left in the bracket!/;
 let percentageNumberRedTeam = 0;
 let strippedDownNumberRedTeam = 0;
 let percentageNumberBlueTeam = 0;
@@ -24,18 +27,28 @@ window.addEventListener('load', function() {
     //alert("It's loaded!")
   
     document.getElementsByClassName("navbar-text")[0].remove();
-    document.getElementById("chat-frame-stream").remove();
-    document.getElementById("balancewrapper").style.visibility = "hidden"; //COMMENT THESE TWO TO CHECK YOUR EARNINGS FOR THE DAY
+    document.getElementById("chat-wrapper").remove();
+    document.getElementById("stream").remove();
+    document.getElementById("balancewrapper").style.visibility = "hidden"; 
     document.getElementById("lastbet").style.visibility = "hidden";
+    document.getElementById("footer-alert").style.color = "white";
+    document.getElementById("footer-alert").style.fontSize = "x-large";
+    document.getElementById("footer-alert").style.margin = "-165px 0px 0px 0px";
+    
   
-    var intervalID = window.setInterval(myCallback, 30000);
+    var intervalID = window.setInterval(myCallback, 5000);
 
 
     function myCallback() {
       
-            console.clear();
-            console.log("Match detected! Betting on the favorite!");
+            document.getElementsByClassName("alerttext")[0].style.display = "none";
 
+
+      
+            console.clear();
+      
+            console.log("Normal match detected! Betting on the favorite!");
+            console.log(document.getElementById("footer-alert").textContent);
             percentageNumberRedTeam = percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML);
             strippedDownNumberRedTeam = strippedDownNumberRegex.exec(percentageNumberRedTeam);
 
@@ -47,16 +60,15 @@ window.addEventListener('load', function() {
             betAmount = Math.round(0.01 * cashAmount);
 
 
-            if (strippedDownNumberRedTeam > strippedDownNumberBlueTeam) 
-            {
+            if (strippedDownNumberRedTeam > strippedDownNumberBlueTeam) {
               
-                document.getElementById("wager").value = betAmount;
-                document.getElementById("player1").click();
+              document.getElementById("wager").value = betAmount;
+              document.getElementById("player1").click();
 
             } 
           
-            else if (strippedDownNumberRedTeam < strippedDownNumberBlueTeam) 
-            {
+              
+            else if (strippedDownNumberRedTeam < strippedDownNumberBlueTeam) {
               
                 document.getElementById("wager").value = betAmount;
                 document.getElementById("player2").click();
@@ -64,6 +76,11 @@ window.addEventListener('load', function() {
             } 
           
             else {}
+
+                
+          }
+
+            
       
             
       
@@ -112,7 +129,7 @@ window.addEventListener('load', function() {
       
         else  { }*/
 
-    }
+    
 
 })
 
