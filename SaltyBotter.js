bdfe7d23851c7ffbe188abd93f79f586d5a1671b
OSad <andreg.95@gmail.com>
2021-02-07 23:35:43 -0300

    Added some methods for readability
    Also removed unused exhibition mode code

--- SaltyBotter.js
+++ SaltyBotter.js
@@ -3,38 +3,32 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.saltybet.com/
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      OSad
 // @description 2/2/2021, 6:37:21 PM
 // ==/UserScript==
 
 
-let percentageNumberRegex = /[0-9][0-9]%/;
-let strippedDownNumberRegex = /[0-9][0-9]/;
-let exhibitionGameModeRegex = /exhibition/;
-let tournamentGameModeStartRegex = /Tournament mode start!/;
-let tournamentGameModeRegexPlural = /[0-9][0-9] characters are left in the bracket!/;
-let tournamentGameModeRegexSingular = /[0-9] characters are left in the bracket!/;
-let percentageNumberRedTeam = 0;
+let percentNumberRedTeam = 0;
 let strippedDownNumberRedTeam = 0;
-let percentageNumberBlueTeam = 0;
+let percentNumberBlueTeam = 0;
 let strippedDownNumberBlueTeam = 0;
 let cashAmount = 0;
 let betAmount = 0;
 
 
+
 window.addEventListener('load', function() {
-    //alert("It's loaded!")
   
     document.getElementsByClassName("navbar-text")[0].remove();
     document.getElementById("chat-wrapper").remove();
-    document.getElementById("stream").remove();
-    document.getElementById("balancewrapper").style.visibility = "hidden"; 
-    document.getElementById("lastbet").style.visibility = "hidden";
+    //document.getElementById("stream").remove();
+    //document.getElementById("balancewrapper").style.visibility = "hidden"; 
+    //document.getElementById("lastbet").style.visibility = "hidden";
     document.getElementById("footer-alert").style.color = "white";
     document.getElementById("footer-alert").style.fontSize = "x-large";
-    document.getElementById("footer-alert").style.margin = "-165px 0px 0px 0px";
-    
+    document.getElementById("footer-alert").style.margin = "-20px 0px 0px 0px";
+  
   
     var intervalID = window.setInterval(myCallback, 5000);
 
@@ -42,18 +36,17 @@ window.addEventListener('load', function() {
     function myCallback() {
       
             document.getElementsByClassName("alerttext")[0].style.display = "none";
-
-
       
-            console.clear();
+            ClearTheDebugConsole();
       
-            console.log("Normal match detected! Betting on the favorite!");
-            console.log(document.getElementById("footer-alert").textContent);
-            percentageNumberRedTeam = percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML);
-            strippedDownNumberRedTeam = strippedDownNumberRegex.exec(percentageNumberRedTeam);
+            PrintToTheDebugConsole("Normal match detected! Betting on the favorite!");
+            PrintToTheDebugConsole(document.getElementById("footer-alert").textContent);
+      
+            percentNumberRedTeam = GetWinrate("bettors1");
+            strippedDownNumberRedTeam = StripPercentSymbolFromWinrateNumber(percentNumberRedTeam)
 
-            percentageNumberBlueTeam = percentageNumberRegex.exec(document.getElementById("bettors2").innerHTML);
-            strippedDownNumberBlueTeam = strippedDownNumberRegex.exec(percentageNumberBlueTeam);
+            percentNumberBlueTeam = GetWinrate("bettors2");
+            strippedDownNumberBlueTeam = StripPercentSymbolFromWinrateNumber(percentNumberBlueTeam);
 
             cashAmount = document.getElementById("balance").textContent;
             cashAmount = parseFloat(cashAmount.replace(/,/g, ''));
@@ -80,56 +73,34 @@ window.addEventListener('load', function() {
                 
           }
 
-            
-      
-            
-      
-      //The following block of code was intended to differentiate between bets in Exhibition game mode and every other game mode.
-      //After some observation, differentiating between bets turned out to be kind of a dumb idea and it's better to just vote for the character most likely to win every time.
-
-
-        /*if (exhibitionGameModeRegex.test(document.getElementById("footer-alert").textContent) === true) 
-        {
-            
-            console.clear();
-            console.log("Exhibs detected! Betting on the favorite!");
-
-
-            percentageNumberRedTeam = percentageNumberRegex.exec(document.getElementById("bettors1").innerHTML);
-            strippedDownNumberRedTeam = strippedDownNumberRegex.exec(percentageNumberRedTeam);
-
-            percentageNumberBlueTeam = percentageNumberRegex.exec(document.getElementById("bettors2").innerHTML);
-            strippedDownNumberBlueTeam = strippedDownNumberRegex.exec(percentageNumberBlueTeam);
-
-            cashAmount = document.getElementById("balance").textContent;
-            cashAmount = parseFloat(cashAmount.replace(/,/g, ''));
-            betAmount = Math.round(0.01 * cashAmount);
-
-
           
-            if (strippedDownNumberRedTeam > strippedDownNumberBlueTeam) 
-            {
-              
-                document.getElementById("wager").value = betAmount;
-                document.getElementById("player1").click();
+function ClearTheDebugConsole()
+  {
+    console.clear();
+  }
+  
+function PrintToTheDebugConsole(stringToPrint)
+  {
+    return console.log(stringToPrint);
+  }
+  
+function GetWinrate(idToGetNumberFrom)
+  {
+    let percentageNumberRegex = /[0-9][0-9]%/;
+    return percentageNumberRegex.exec(document.getElementById(idToGetNumberFrom).innerHTML);
+  }
 
-            } 
-          
-            else if (strippedDownNumberRedTeam < strippedDownNumberBlueTeam) 
-            
-            {
-                document.getElementById("wager").value = betAmount;
-                document.getElementById("player2").click();
-
-            } 
-          
-            else {}
-        } 
-      
-      
-        else  { }*/
-
-    
+function StripPercentSymbolFromWinrateNumber(winrateWithPercentageSymbol)
+  {
+    let strippedDownNumberRegex = /[0-9][0-9]/;
+    return strippedDownNumberRegex.exec(winrateWithPercentageSymbol);
+  }
+  
+function GetCash(idToGetCashFrom)
+  {
+    document.getElementById(idToGetCashFrom).textContent;
+    return parseFloat(idToGetCashFrom.replace(/,/g, ''));
+  }
 
 })
 
